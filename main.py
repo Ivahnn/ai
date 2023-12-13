49f221c134beea6df0f4892f52dd004c6d9ec959
Ivahnn Garcia <migarcia@tip.edu.ph>
2023-12-13 18:46:14 +0800

    patulong pre

---
+++ main.py
@@ -0,0 +1,96 @@
+import random
+import cv2
+import cvzone
+from cvzone.HandTrackingModule import HandDetector
+import time
+import csv
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+detector = HandDetector(maxHands=1)
+
+timer = 0
+stateResult = False
+startGame = False
+scores = [0, 0]  # [AI, Player]
+
+csv_filename = 'scores.csv'
+
+def show_result_popup(result):
+    cv2.rectangle(imgBG, (300, 150), (900, 350), (0, 255, 0), cv2.FILLED)
+    cv2.putText(imgBG, result, (400, 250), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 3)
+    cv2.imshow("BG", imgBG)
+    cv2.waitKey(2000)  # 2-second delay
+
+while True:
+    imgBG = cv2.imread("Resources/BG.png")
+    success, img = cap.read()
+
+    imgScaled = cv2.resize(img, (0, 0), None, 0.875, 0.875)
+    imgScaled = imgScaled[:, 80:480]
+
+    # Find Hands
+    hands, img = detector.findHands(imgScaled)  # with draw
+
+    if startGame:
+
+        if stateResult is False:
+            timer = time.time() - initialTime
+            cv2.putText(imgBG, str(int(timer)), (605, 435), cv2.FONT_HERSHEY_PLAIN, 6, (255, 0, 255), 4)
+
+            if timer > 3:
+                stateResult = True
+                timer = 0
+
+                if hands:
+                    playerMove = None
+                    hand = hands[0]
+                    fingers = detector.fingersUp(hand)
+                    if fingers == [0, 0, 0, 0, 0]:
+                        playerMove = 1
+                    if fingers == [1, 1, 1, 1, 1]:
+                        playerMove = 2
+                    if fingers == [0, 1, 1, 0, 0]:
+                        playerMove = 3
+
+                    randomNumber = random.randint(1, 3)
+                    imgAI = cv2.imread(f'Resources/{randomNumber}.png', cv2.IMREAD_UNCHANGED)
+                    imgBG = cvzone.overlayPNG(imgBG, imgAI, (149, 310))
+
+                    # Player Wins
+                    if (playerMove == 1 and randomNumber == 3) or \
+                            (playerMove == 2 and randomNumber == 1) or \
+                            (playerMove == 3 and randomNumber == 2):
+                        scores[1] += 1
+                        show_result_popup("PLAYER WON")
+
+                    # AI Wins
+                    if (playerMove == 3 and randomNumber == 1) or \
+                            (playerMove == 1 and randomNumber == 2) or \
+                            (playerMove == 2 and randomNumber == 3):
+                        scores[0] += 1
+                        show_result_popup("AI WON")
+
+    imgBG[234:654, 795:1195] = imgScaled
+
+    if stateResult:
+        imgBG = cvzone.overlayPNG(imgBG, imgAI, (149, 310))
+
+    cv2.putText(imgBG, str(scores[0]), (410, 215), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 6)
+    cv2.putText(imgBG, str(scores[1]), (1112, 215), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 6)
+
+    # Update CSV file with scores
+    with open(csv_filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['AI', 'Player'])  # Write header
+        writer.writerow([scores[0], scores[1]])  # Write scores
+
+    cv2.imshow("BG", imgBG)
+
+    key = cv2.waitKey(1)
+    if key == ord('s'):
+        startGame = True
+        initialTime = time.time()
+        stateResult = False
